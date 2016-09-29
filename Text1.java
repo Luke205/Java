74f6de0f7afeb9bb608d5804dc3f22d2574f336c
Luke205 <dogblue01@gmail.com>
2016-09-29 13:57:44 -0700

    Add files via upload

---
+++ Text1.java
@@ -0,0 +1,23 @@
+class Text1 {
+
+	public static void main (string a[]){
+			int i, ci;	// integer 1
+			char c;	  // charater c
+
+	i= 7;
+	c = 'A';
+	s = "data";
+	System.out.println("-------------------------------");
+	system.out.println(i);
+	System.out.println(c);
+	System.out.println(s);
+		System.out.println("--------------------------------");
+	//do some math on the variables
+	i = i';'
+	
+	System.out.println("-------------------------------");
+	System.out.println(i);
+	System.out.println(ci);
+	
+	}
+}
